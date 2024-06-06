c5fa04f731d2adc5879f0ad659b4cb04ac7ed150
gamagoat <35706274+gamagoat@users.noreply.github.com>
2024-06-06 12:17:12 +0900

    Use mapleader for nvim keymaps

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,2 +1,12 @@
-vim.keymap.set("n", " pv", vim.cmd.Ex)
+-- Use <Space> as a leader
+vim.g.mapleader = " "
+
+local keymap_opts = {
+  -- No recursive mappings
+  noremap = true,
+}
+
+-- Open project view
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
 
