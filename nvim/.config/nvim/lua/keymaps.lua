048ba72221cfc6f197efbbebf4737322e6e79125
gamagoat <35706274+gamagoat@users.noreply.github.com>
2024-06-06 14:01:37 +0900

    Add telescope to nvim

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,10 +1,7 @@
+local utils = require("utils")
+
 -- Use <Space> as a leader
 vim.g.mapleader = " "
-
-local keymap_opts = {
-  -- No recursive mappings
-  noremap = true,
-}
 
 -- Open project view
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
@@ -12,4 +9,3 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- Copy to system clipboard
 vim.keymap.set('n', '<leader>y', '"+y')
 vim.keymap.set('v', '<leader>y', '"+y')
-
--- nvim/.config/nvim/lua/launch.lua
+++ nvim/.config/nvim/lua/launch.lua
@@ -1,9 +1,52 @@
 -- Plugins go in this table
 LAZY_PLUGIN_SPEC = {
+  -- Glow
   {"ellisonleao/glow.nvim", config = true, cmd = "Glow"},
-  {{
+  
+ -- Telescope
+ {
+   "nvim-telescope/telescope.nvim",
+   dependencies = { 'nvim-lua/plenary.nvim' },
+   lazy = false,
+   cmd = "Telescope",
+   config = function ()
+    local builtin = require "telescope.builtin"
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+    local actions = require "telescope.actions"
+    require("telescope").setup{
+        defaults = {
+            mappings = {
+                i = {
+                  ["<C-n>"] = actions.cycle_history_next,
+                  ["<C-p>"] = actions.cycle_history_prev,
+        
+                  ["<C-j>"] = actions.move_selection_next,
+                  ["<C-k>"] = actions.move_selection_previous,
+
+                  ["<C-q>"] = actions.close,
+                },
+                n = {
+                  ["<esc>"] = actions.close,
+                  ["j"] = actions.move_selection_next,
+                  ["k"] = actions.move_selection_previous,
+                  ["q"] = actions.close,
+                },
+              },
+        },
+      }
+
+    end
+ },
+
+  -- Treesitter
+  {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    lazy = false,
     config = function () 
       local configs = require("nvim-treesitter.configs")
 
@@ -20,12 +63,10 @@ LAZY_PLUGIN_SPEC = {
           indent = { enable = true },  
         })
     end
- }},
+ },
+
 }
 
 function spec(item)
 	table.insert(LAZY_PLUGIN_SPEC, { import = item })
 end
-
-
-
