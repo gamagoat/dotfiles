90bafc92a8210daa1a4c54dedce1d70ce6f80289
gamagoat <35706274+gamagoat@users.noreply.github.com>
2024-06-06 12:44:05 +0900

    Add system clipboard copy keymaps

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -9,4 +9,7 @@ local keymap_opts = {
 -- Open project view
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Copy to system clipboard
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')
 
