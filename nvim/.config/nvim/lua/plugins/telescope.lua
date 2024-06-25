92c01c587aa15fa85c6b0b40cc89994766e108f5
gamagoat <35706274+gamagoat@users.noreply.github.com>
2024-06-25 18:01:47 +0900

    Add file browser plugin to telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,38 +1,56 @@
 local builtin = require("telescope.builtin")
-local actions = require("telescope.actions")
 
 return {
-  "nvim-telescope/telescope.nvim",
+  {
+    "nvim-telescope/telescope.nvim",
 
-  keys = {
-    -- Override fg to grep files
-    { "<leader>fg", builtin.live_grep, desc = "Grep files" },
+    keys = {
+      -- Override fg to grep files
+      { "<leader>fg", builtin.live_grep, desc = "Grep files" },
 
-    -- Disable the old grep keymap
-    { "<leader>/", false },
+      -- Disable the old grep keymap
+      { "<leader>/", false },
+    },
+
+    defaults = {
+      -- Arguments to pass when grepping
+      vimgrep_arguments = {
+        "rg",
+        -- Show color in output if in an interactive terminal
+        "--color=auto",
+        -- Prevent printing filenames above matching content
+        "--no-heading",
+        -- Include filename in the output
+        "--with-filename",
+        -- Include line number in the output
+        "--line-number",
+        -- Include column number in the output
+        "--column",
+        -- Case-insensitive search if pattern is all lowercase, case-sensitive otherwise
+        "--smart-case",
+        -- Include hidden files and directories in the search
+        "--hidden",
+        -- Exclude .git directories and their contents
+        "--glob",
+        "!.git/*",
+      },
+    },
   },
 
-  defaults = {
-    -- Arguments to pass when grepping
-    vimgrep_arguments = {
-      "rg",
-      -- Show color in output if in an interactive terminal
-      "--color=auto",
-      -- Prevent printing filenames above matching content
-      "--no-heading",
-      -- Include filename in the output
-      "--with-filename",
-      -- Include line number in the output
-      "--line-number",
-      -- Include column number in the output
-      "--column",
-      -- Case-insensitive search if pattern is all lowercase, case-sensitive otherwise
-      "--smart-case",
-      -- Include hidden files and directories in the search
-      "--hidden",
-      -- Exclude .git directories and their contents
-      "--glob",
-      "!.git/*",
+  -- Plugins for telescope
+
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    keys = {
+      {
+        "<leader>sB",
+        ":Telescope file_browser path=%:p:h=%:p:h<cr>",
+        desc = "Browse Files",
+      },
     },
+    config = function()
+      require("telescope").load_extension("file_browser")
+    end,
   },
 }
