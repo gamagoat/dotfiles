1d51a1317aafb50537b7b54db31ceea7761325c5
gamagoat <35706274+gamagoat@users.noreply.github.com>
2024-09-04 21:13:52 +0900

    Reduce conform formatters to only md

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -3,8 +3,7 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        ["markdown"] = { { "prettierd", "prettier" }, "markdownlint", "markdown-toc" },
-        ["markdown.mdx"] = { { "prettierd", "prettier" }, "markdownlint", "markdown-toc" },
+        ["markdown"] = { "prettierd", "markdownlint", "markdown-toc" },
       },
     },
   },
