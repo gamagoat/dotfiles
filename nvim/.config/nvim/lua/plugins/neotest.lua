ba659fbfb6b9aa3f33fb82e6ca2b40249ccc8f8a
gamagoat <35706274+gamagoat@users.noreply.github.com>
2024-10-24 22:50:00 +0900

    Add neotest config

---
+++ nvim/.config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-neotest/neotest-python",
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-python"),
+        },
+      })
+    end,
+  },
+}
