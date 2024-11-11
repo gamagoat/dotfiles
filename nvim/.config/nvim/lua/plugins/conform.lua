fa1e4ca8cdfdc5c1ba421936439814394c28808d
gamagoat <35706274+gamagoat@users.noreply.github.com>
2024-11-11 11:55:27 +0900

    Add cue support

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -5,6 +5,7 @@ return {
       formatters_by_ft = {
         ["markdown"] = { "prettierd", "markdownlint", "markdown-toc" },
         ["sh"] = { "shfmt" },
+        ["cue"] = { "cue_fmt" },
       },
     },
   },
